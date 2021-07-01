aadded0d7b8688277e798ddd21909dccec7f7653
Donovan <divdonis@gmail.com>
2021-06-30 20:14:37 -0700

    Completed Assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,13 @@
-// Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe(){
+    function focusFunc(){
+        console.log('Good!')
+    };
+    function blurFunc(){
+        console.log('Hey! Eyes on me!')
+    };
+    return <button onFocus={focusFunc} onBlur={blurFunc}>Eyes on me</button>
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,11 @@
-// Code Keypad Component Here
+import React from "react";
+
+function Keypad(){
+    function keypad(){
+        console.log('Entering password...')
+    }
+    return <input onChange={keypad} type="password" />
+    
+}
+
+export default Keypad
